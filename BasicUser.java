b331860bac741c6cad52b4e138bb269ff3ca1a0f
MichelVelasco <lmvg.1989@gmail.com>
2021-08-24 15:14:18 -0500

    Creating a new simple object from a SubClass

--- BasicUser.java
+++ BasicUser.java
@@ -1,12 +1,16 @@
-class Basicuser extends User {
+class BasicUser extends User {
     String membershipType;
     Integer devices;
 
-    public Basicuser (Integer id, String name, String membershipType, Integer devices) {
+    public BasicUser (Integer id, String name, String membershipType, Integer devices) {
         super(id, name);
         this.membershipType = membershipType;
         this.devices = devices;
 
     }
     
+    public void printBasicUser(){
+        super.printUser();
+        System.out.println("Membership Type: " + membershipType + "\nDevices allowed: " + devices);
+    }
 }
--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main(String[] args){
 
-        User usuario = new User (2121, "Michel"); 
-        usuario.printUser();
+        //User usuario = new User (2121, "Michel"); 
+        //usuario.printUser();
+       
+        BasicUser freeUser;
+        freeUser = new BasicUser(1989 , "Velasco", "Free", 1);
+        freeUser.printBasicUser();
     }
 
 }
